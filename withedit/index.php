c1d4abf2bf67cc73d910ea3db33a8a1f47619cdf
Clint Harvey <clint@voceconnect.com>
2016-10-07 08:09:10 -0500

    polish

--- withedit/index.php
+++ withedit/index.php
@@ -8,6 +8,8 @@ $db = new PDO('mysql:host=' . $db_info['server'] . ';dbname=' . $db_info['name']
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">meta charset="utf-8">
 		<title>Todo</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		
